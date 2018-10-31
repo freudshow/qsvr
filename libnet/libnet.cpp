e58503b7b27b252b36f699bded75ebe7fb5aa77e
s_baoshan <s_baoshan@163.com>
2018-10-31 11:21:56 +0800

    libnet

--- libnet/libnet.cpp
+++ libnet/libnet.cpp
@@ -1,16 +1,80 @@
 #include "libnet.h"
 
-libnet::libnet(QObject *parent) : QObject(parent)
+threadObj::threadObj(qintptr ID, QObject *parent) :
+    QThread(parent)
 {
-
+    this->socketDescriptor = ID;
 }
 
-libnet::~libnet()
+void threadObj::run()
 {
+    qDebug() << " Thread started: " << QThread::currentThreadId();
 
+    m_socket = new QTcpSocket();
+    connect(this, SIGNAL(error()), this, SLOT(deleteLater()));
+    if(!m_socket->setSocketDescriptor(this->socketDescriptor)) {
+        emit error(m_socket->error());
+        return;
+    }
+
+    connect(m_socket, SIGNAL(readyRead()), this, SLOT(readyRead()), Qt::DirectConnection);
+    connect(m_socket, SIGNAL(disconnected()), this, SLOT(disconnected()));
+
+    qDebug() << "socketDescriptor: " << socketDescriptor << " Client connected";
+    qDebug() << "Client:" << m_socket->peerAddress()
+             << m_socket->peerPort() << m_socket->peerName()
+             << "connect!";
+
+    exec();
 }
 
-void libnet::startThread()
+void threadObj::readyRead()
 {
+    QByteArray b = m_socket->readAll();
+    qDebug() << socketDescriptor << " Data in: " << b;
+    emit dataIn(b);
+}
 
+void threadObj::sendFrame(QByteArray b)
+{
+    m_socket->write(b);
+}
+
+void threadObj::disconnected()
+{
+    qDebug() << socketDescriptor << " Disconnected";
+
+    m_socket->deleteLater();
+    exit(0);
+}
+
+netSvr::netSvr(QObject *parent) :
+    QTcpServer(parent)
+{
+}
+
+bool netSvr::startServer(quint16 port)
+{
+    if(!this->listen(QHostAddress::Any, port)) {
+        qDebug() << "Could not start server" << this->errorString();
+        return false;
+    }
+
+    qDebug() << "Listening to port " << port << "...";
+    return false;
+}
+
+void netSvr::incomingConnection(qintptr socketDescriptor)
+{
+    qDebug() << socketDescriptor << " Connecting...";
+
+    threadObj *thread = new threadObj(socketDescriptor, this);
+    connect(thread, SIGNAL(finished()), thread, SLOT(deleteLater()));
+    connect(thread, SIGNAL(error(QTcpSocket::SocketError)), this, SLOT(socketError(QTcpSocket::SocketError)));
+    thread->start();
+}
+
+void netSvr::socketError(QTcpSocket::SocketError err)
+{
+    qDebug() << "socketError";
 }
--- libnet/libnet.h
+++ libnet/libnet.h
@@ -1,22 +1,54 @@
 #ifndef LIBNET_H
 #define LIBNET_H
 
+/*
+ * reference: https://www.bogotobogo.com/Qt/Qt5_QTcpServer_Multithreaded_Client_Server.php
+ *            https://www.bogotobogo.com/Qt/Qt5_QTcpServer_Client_Server.php
+ */
 #include <QObject>
 #include <QTcpServer>
 #include <QTcpSocket>
+#include <QThread>
 #include "libutil/basedef.h"
 
-class libnet : public QObject
+
+class threadObj : public QThread
+{
+    Q_OBJECT
+
+public:
+    explicit threadObj(qintptr ID, QObject *parent = Q_NULLPTR);
+
+    void run();
+
+signals:
+    void dataIn(QByteArray);
+    void error(QTcpSocket::SocketError socketerror);
+
+public slots:
+    void readyRead();
+    void sendFrame(QByteArray);
+    void disconnected();
+
+private:
+    QTcpSocket *m_socket;
+    qintptr socketDescriptor;
+};
+
+class netSvr : public QTcpServer
 {
     Q_OBJECT
 public:
-    explicit libnet(QObject *parent = nullptr);
-    ~libnet();
-
+    explicit netSvr(QObject *parent = Q_NULLPTR);
+    bool startServer(quint16);
 signals:
 
 public slots:
-    void startThread();
+    void socketError(QTcpSocket::SocketError);
+
+protected:
+    void incomingConnection(qintptr socketDescriptor);
+
 };
 
 #endif // LIBNET_H
