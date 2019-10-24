8b0c2dde4eae6604c751b49c760f2e525e5fb2e3
s_baoshan <s_baoshan@163.com>
2019-10-24 21:40:09 +0800

    split update file into blocks

--- update698.cpp
+++ update698.cpp
@@ -10,6 +10,12 @@ update698::update698(QWidget *parent) :
 {
     ui->setupUi(this);
     connect(ui->btn_browse, SIGNAL(clicked(bool)), this, SLOT(browse()));
+
+    m_oneBlockSize = 512;
+    ui->buttonGroup->setId(ui->rbtn_256, 0);
+    ui->buttonGroup->setId(ui->rbtn_512, 1);
+    ui->buttonGroup->setId(ui->rbtn_1024, 2);
+    QObject::connect(ui->buttonGroup, SIGNAL(buttonClicked(int)), this, SLOT(btnToggle(int)));
 }
 
 update698::~update698()
@@ -22,8 +28,27 @@ void update698::on_btn_exit_clicked(bool)
     this->close();
 }
 
+void update698::btnToggle(int i)
+{
+    switch (i) {
+    case 0:
+        m_oneBlockSize = 256;
+        break;
+    case 1:
+        m_oneBlockSize = 512;
+        break;
+    case 2:
+        m_oneBlockSize = 1024;
+        break;
+    default:
+        m_oneBlockSize = 512;
+        break;
+    }
+}
+
 void update698::browse()
 {
+    int i = 0;
     QString filename =  QFileDialog::getOpenFileName(this, tr("Open File"),
                                                QDir::homePath(),
                                                tr("all file (*)"));
@@ -39,6 +64,11 @@ void update698::browse()
     file.seek(0);
     m_filesize = file.size();
     m_fileblock = file.readAll();
-//    m_fileblock.mid(i, len);
+    m_blockCount = m_filesize/m_oneBlockSize + (((m_filesize%m_oneBlockSize)==0) ? 0 : 1);
+    m_blockList.clear();
+
+    for (i = 0; i < m_blockCount; i++)
+        m_blockList.append(m_fileblock.mid(i, m_oneBlockSize));
+
     file.close();
 }
--- update698.h
+++ update698.h
@@ -23,13 +23,15 @@ private:
     Ui::update698 *ui;
 
     QList<QByteArray> m_blockList;
-    QByteArray m_fileblock;
-    qint64 m_filesize;
-    qint64 m_blockCount;
+    QByteArray m_fileblock; //whole file bytes
+    qint64 m_filesize;      //whole file length
+    qint64 m_oneBlockSize;  //size of per file block
+    qint64 m_blockCount;    //counts of update file
 
 private slots:
     void browse();
     void on_btn_exit_clicked(bool);
+    void btnToggle(int);
 };
 
 #endif // UPDATE698_H
